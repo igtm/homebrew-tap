f0bc35aa6f82c8005a0d56f80f4a2ebf21901e5e
Tomokatsu Iguchi <tomo14916253@gmail.com>
2022-12-30 21:20:55 +0900

    bump openapi-yup-generator v0.0.7

--- openapi-yup-generator.rb
+++ openapi-yup-generator.rb
@@ -4,20 +4,20 @@
 class OpenapiYupGenerator < Formula
   desc "CLI tool for generating yup definitions from openapi3.yaml"
   homepage "https://github.com/igtm/openapi-yup-generator"
-  version "0.0.6"
+  version "0.0.7"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/igtm/openapi-yup-generator/releases/download/v0.0.6/openapi-yup-generator_v0.0.6_x86_64-apple-darwin.tar.gz"
-      sha256 "e9257542336aeb815e629df05107affc35cc7e81ce970254105db3da240f9cda"
+      url "https://github.com/igtm/openapi-yup-generator/releases/download/v0.0.7/openapi-yup-generator_v0.0.7_x86_64-apple-darwin.tar.gz"
+      sha256 "29addc364ae1be35072dfad68b5a88b1f20e388d721ae98dd6aa341166d46078"
 
       def install
         bin.install "openapi-yup-generator"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/igtm/openapi-yup-generator/releases/download/v0.0.6/openapi-yup-generator_v0.0.6_arm64-apple-darwin.tar.gz"
-      sha256 "9bc37f12f9099eda887ac3e51cbe2f5a6a81d7e0dad15e15cb0fadab0040f0e0"
+      url "https://github.com/igtm/openapi-yup-generator/releases/download/v0.0.7/openapi-yup-generator_v0.0.7_aarch64-apple-darwin.tar.gz"
+      sha256 "c22a7d9120c8ff04ecd355a244f937ec966c5db01d4db0b2def8b08eeb675090"
 
       def install
         bin.install "openapi-yup-generator"
@@ -27,20 +27,20 @@ class OpenapiYupGenerator < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/igtm/openapi-yup-generator/releases/download/v0.0.6/openapi-yup-generator_v0.0.6_x86_64-unknown-linux-musl.tar.gz"
-      sha256 "1ba366435a6036136dd6a0a1ec7fbf4e81a55181bd81c5e59a5a9b572f192da8"
+      url "https://github.com/igtm/openapi-yup-generator/releases/download/v0.0.7/openapi-yup-generator_v0.0.7_x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "6cbb1a290fcee8de760d5f651801d80fb5984a85a433d944cbbbbc15d3d9113a"
 
       def install
         bin.install "openapi-yup-generator"
       end
     end
-    # if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    #   url "https://github.com/igtm/openapi-yup-generator/releases/download/v0.0.6/openapi-yup-generator_v0.0.6_x86_64-unknown-linux-musl.tar.gz"
-    #   sha256 "81286b9c2bb78f9b696033267e8493494dc0dba916dce304afe3e33ed9330b99"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/igtm/openapi-yup-generator/releases/download/v0.0.7/openapi-yup-generator_v0.0.7_aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "c6cd4754bfcbd32b2d0b75f1fc61ad98a53c17c3721638faca5baa7e2774e934"
 
-    #   def install
-    #     bin.install "openapi-yup-generator"
-    #   end
-    # end
+      def install
+        bin.install "openapi-yup-generator"
+      end
+    end
   end
 end
